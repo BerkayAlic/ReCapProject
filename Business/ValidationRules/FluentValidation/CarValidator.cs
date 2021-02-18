22896dbf9334c094f41585a4c43fb0c0ad5cf7d8
berkay <berkayalic2@gmail.com>
2021-02-19 01:08:19 +0300

    12. Gun Odevler

---
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -0,0 +1,29 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CarValidator : AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(c => c.CarName).NotEmpty();
+            RuleFor(c => c.CarName).MinimumLength(2);
+            RuleFor(c => c.DailyPrice).NotEmpty();
+            RuleFor(c => c.DailyPrice).GreaterThan(0);
+            RuleFor(c => c.CarName).Must(StartWithA).WithMessage("CarName should start with A");
+
+
+        }
+
+        private bool StartWithA(string arg)
+        {
+            return arg.StartsWith("A");
+        }
+
+        //arg is the parameter is sent which is CarName
+    }
+}
